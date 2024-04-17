08eb08aac91576fcff390256c0090d319e0466c1
Omar Soriano <osoriano2@gmail.com>
2024-04-17 08:39:08 -0700

    Add nvm env vars
    nvm docs: https://github.com/nvm-sh/nvm\#git-install
    
    One difference is the install path

--- env.sh
+++ env.sh
@@ -3,3 +3,9 @@ export EDITOR="$VISUAL"
 
 # fd uses .gitignore files to filter file results
 export FZF_DEFAULT_COMMAND='fd'
+
+# From nvm git install instructions
+# See https://github.com/nvm-sh/nvm#git-install
+export NVM_DIR="$HOME/gh/nvm-sh/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
